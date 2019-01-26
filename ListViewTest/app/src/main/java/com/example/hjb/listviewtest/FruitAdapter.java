a77fa945d94ea5cf4710afaf7bbbbf87f9eaf722
郭思义 <gsy@meitu.com>
2019-01-26 23:22:36 +0800

    [optimize] 格式化代码，修复编译不过的问题

--- ListViewTest/app/src/main/java/com/example/hjb/listviewtest/FruitAdapter.java
+++ ListViewTest/app/src/main/java/com/example/hjb/listviewtest/FruitAdapter.java
@@ -18,21 +18,23 @@ import java.util.List;
  * @updateDes ${TODO}
  */
 public class FruitAdapter extends ArrayAdapter<Fruit> {
+
     private int resourceId;
-    public FruitAdapter(Context context, int textViewResourceId, List<Fruit> objects);
-    {
-        super(context,textViewResourceId,objects);
-    resourceId = textViewResourceId;
-}
-@Override
-public View getView(int position,View convertView,ViewGroup parent) {
-    Fruit fruit = getItem(position);
-    View view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
-    ImageView fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
-    TextView fruitName = (TextView) view.findViewById(R.id.fruit_name);
-    fruitImage.setImageResource(fruit.getImageId());
-    fruitName.setText(fruit.getName());
-    return view;
+
+    public FruitAdapter(Context context, int textViewResourceId, List<Fruit> objects) {
+        super(context, textViewResourceId, objects);
+        resourceId = textViewResourceId;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Fruit fruit = getItem(position);
+        View view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+        ImageView fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+        TextView fruitName = (TextView) view.findViewById(R.id.fruit_name);
+        fruitImage.setImageResource(fruit.getImageId());
+        fruitName.setText(fruit.getName());
+        return view;
 
     }
 }
